8b4933c30185f4332b86139f08fa324e2e4b774c
Tom Rainford <tom_rainford@yahoo.co.uk>
2022-09-22 22:13:07 +0100

    Update #1
    - Added further functionality to web app
    - Cleaned up code
    - Updated readme

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # RustWebApp
- A test web application built with Rust
+🦀 A test web counter built with Rust
+
+To Run: 
+1. Clone Repository or Download
+2. Open folder within repository through command prompt
+3. Use `trunk serve` to run locally
+
--- RustWebApp/src/main.rs
+++ RustWebApp/src/main.rs
@@ -1,18 +1,17 @@
 use yew::prelude::*;
 
-
 struct Model {
     value: i64
 }
 
-
+ 
 #[function_component(App)]
 fn app() -> Html {
     let state = use_state(|| Model{
         value: 0
     });
 
-    let onclick ={
+    let onclick_add = {
         let state = state.clone();
 
         Callback::from(move |_| {
@@ -22,11 +21,21 @@ fn app() -> Html {
         })
     };
 
+    let onclick_minus = {
+        let state = state.clone();
+
+        Callback::from(move |_| {
+            state.set(Model {
+                value: state.value - 1
+            })
+        })
+    };
 
     html!{
 
         <div>
-            <button {onclick}>{"+1"}</button>
+            <button id="addBtn" onclick={onclick_add}>{"+1"}</button>
+            <button id="minusBtn" onclick={onclick_minus}>{"-1"}</button>
             <p>{state.value}</p>
 
         </div>
